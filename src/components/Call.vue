f715216cb264cbbfdd6fc99b7387e3fc4236a1b1
shivangi-chhabra <40178102+shivangi-chhabra@users.noreply.github.com>
2020-05-26 00:52:07 +0530

    some change

---
+++ src/components/Call.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="col-sm-3">
+    <h2 class="sub_title w10">CALL YOU</h2>
+    <div class="clearfix"></div>
+    <div class="login-form-1">
+        <form id="login-form" class="text-left">
+            <div class="login-form-main-message"></div>
+            <div class="main-login-form">
+                <div class="login-group">
+                    <div class="form-group">
+                        <label for="ad" class="sr-only">Name</label>
+                        <input type="text" class="form-control" id="ad"  name="ad" v-model="People.ad" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="tel" class="sr-only">Phone Number</label>
+                        <input type="text" class="form-control" id="tel" name="tel" v-model="People.tel" placeholder="Phone Number">
+                    </div>
+                </div>
+                <button id="myForm" type="submit" class="login-button" @click="addToAPI"><i class="fa fa-chevron-right"></i></button>
+            </div>
+        </form>
+    </div>                            
+     </div>
+    
+</template>
+<script>
+import $ from'jquery'
+import axios from 'axios'
+  export default {
+  name: 'Home',
+  props: {
+    
+  },
+  data(){
+
+      return{
+          People:{ad:'',tel:''}
+      }
+
+  },
+  methods:{
+      addToAPI(){
+
+        let newPerson = {
+            ad: this.People.ad,
+            tel: this.People.tel
+        }
+
+        console.log(newPerson);
+
+        axios.post('')
+        .then((response)=>{ console.log(response);
+        })
+        .catch((error) => { console.log(error);
+        });
+
+    }
+
+  },
+     mounted(){
+
+       $("#login-form").submit(function(e) {
+    e.preventDefault();
+});
+}      
+}  
+ </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -255,28 +255,7 @@
                                 </div>
                             </div>                            
                         </div>
-                        <div class="col-sm-3">
-                            <h2 class="sub_title w10">CALL YOU</h2>
-                            <div class="clearfix"></div>
-                            <div class="login-form-1">
-                                <form id="login-form" class="text-left">
-                                    <div class="login-form-main-message"></div>
-                                    <div class="main-login-form">
-                                        <div class="login-group">
-                                            <div class="form-group">
-                                                <label for="ad" class="sr-only">Name</label>
-                                                <input type="text" class="form-control" id="ad" name="ad" placeholder="Name">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="tel" class="sr-only">Phone Number</label>
-                                                <input type="text" class="form-control" id="tel" name="tel" placeholder="Phone Number">
-                                            </div>
-                                        </div>
-                                        <button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
-                                    </div>
-                                </form>
-                            </div>                            
-                        </div>
+                        <Call />
                     </div>
                 </div>
             </div>
@@ -284,19 +263,53 @@
     </main>
 </template>
 
-<script >
+<script>
 import $ from "jquery"
 import 'bootstrap'
-
+import axios from 'axios' //eslint-disable-line
+import Call from '@/components/Call.vue' 
   export default {
   name: 'Home',
+  components: {
+      Call
+     
+      
+  },
+
   props: {
     
   },
-  data: () => ({
-        
-    }),
+  data(){
+
+      return{
+          People:{ad:'',tel:''}
+      }
+
+  },
+  methods:{
+      addToAPI(){
+
+        let newPerson = {
+            ad: this.People.ad,
+            tel: this.People.tel
+        }
+
+        console.log(newPerson);
+
+        axios.post('')
+        .then((response)=>{ console.log(response);
+        })
+        .catch((error) => { console.log(error);
+        });
+
+    }
+
+  },
      mounted(){
+
+         $("#prospects_form").submit(function(e) {
+    e.preventDefault();
+});
       
    $('.carousel[data-type="multi"] .item').each(function(){
        $('.carousel').carousel({
@@ -316,10 +329,11 @@ import 'bootstrap'
 
             this.next.children(':first-child').clone().appendTo($(this));
           }
-        });  
-        
-        
+        });    
     }
+
+    
+
    
 }  
     </script>
